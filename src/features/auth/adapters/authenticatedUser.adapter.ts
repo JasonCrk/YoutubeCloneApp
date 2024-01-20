4cdceb85168fbef5100afe2d876f02a6b1ce09b4
JasonCrk <emerzonkolki@gmail.com>
2024-01-20 16:59:17 -0500

    feat(features/auth): add email property for authenticated user adapter

--- src/features/auth/adapters/authenticatedUser.adapter.ts
+++ src/features/auth/adapters/authenticatedUser.adapter.ts
@@ -2,11 +2,13 @@ import {
   AuthenticatedUser,
   AuthenticatedUserAdapter
 } from '@/features/auth/models'
+
 import { currentChannelAdapter } from '@/features/channel/adapters'
 
 export const authenticatedUserAdapter = (
   user: AuthenticatedUser
 ): AuthenticatedUserAdapter => ({
   id: user.id,
+  email: user.email,
   currentChannel: currentChannelAdapter(user.current_channel)
 })
--- src/features/auth/models/AuthenticatedUser.model.ts
+++ src/features/auth/models/AuthenticatedUser.model.ts
@@ -14,5 +14,6 @@ export interface AuthenticatedUser {
 
 export interface AuthenticatedUserAdapter {
   id: UserId
+  email: UserEmail
   currentChannel: CurrentChannelAdapter
 }
