d2ba746262c991255d599576f9128e8b49dcfc72
JasonCrk <emerzonkolki@gmail.com>
2024-01-27 16:47:16 -0500

    test(SearchField.test.tsx): add tests and remove

--- src/components/form/SearchField/SearchField.test.tsx
+++ src/components/form/SearchField/SearchField.test.tsx
@@ -1,60 +1,114 @@
 import { cleanup, screen } from '@testing-library/react'
-
 import { userEvent } from '@testing-library/user-event'
 
 import SearchField from '@/components/form/SearchField'
 
 import { renderWithRouter } from '@/utils/testing/routerRender'
 
+import { useNavigate } from 'react-router-dom'
+
 describe('<SearchField />', () => {
-  afterEach(cleanup)
+  vi.mock('react-router-dom', async () => {
+    const actual = await vi.importActual('react-router-dom')
+    return {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ...(actual as any),
+      useNavigate: vi.fn()
+    }
+  })
+
+  const useNavigateMock = vi.mocked(useNavigate)
+
+  afterEach(() => {
+    cleanup()
+  })
 
   it('Should render component', () => {
     renderWithRouter(<SearchField />)
 
     const searchFieldComponent = screen.queryByTestId('SearchField')
-
     expect(searchFieldComponent).toBeInTheDocument()
   })
 
-  it('Should contain a input element', () => {
+  it('Should the input search contain a empty text value by default', () => {
     renderWithRouter(<SearchField />)
 
-    const inputSearch = screen.getByRole('search')
+    const inputSearch = screen.getByPlaceholderText('Search')
 
-    expect(inputSearch.tagName).toBe('INPUT')
+    expect(inputSearch).toHaveTextContent('')
   })
 
-  it('Should the input element have placeholder', () => {
+  it('Should contain the search button', () => {
     renderWithRouter(<SearchField />)
 
-    const inputSearch = screen.getByRole('search')
-    expect(inputSearch).toHaveAttribute('placeholder', 'Search')
+    const searchButton = screen.queryByRole('button')
+    expect(searchButton).toBeInTheDocument()
   })
 
-  it('Should contain a button element', () => {
+  it('Should show the search icon if the user is focused on the input search', async () => {
     renderWithRouter(<SearchField />)
+    const user = userEvent.setup()
 
-    const searchButton = screen.getByRole('button')
-    expect(searchButton.tagName).toBe('BUTTON')
+    const inputSearch = screen.getByPlaceholderText('Search')
+
+    await user.click(inputSearch)
+
+    const searchIcon = screen.queryByTestId('searchIcon')
+    expect(searchIcon).toBeInTheDocument()
   })
 
-  it('Should hide the search icon when the user is not focused on the input search', async () => {
+  it("Shouldn't show the search icon if the user is not focused on the input search", async () => {
     renderWithRouter(<SearchField />)
 
     const searchIcon = screen.queryByTestId('searchIcon')
     expect(searchIcon).toBeNull()
   })
 
-  it('Should show the search icon when the user is focused on the input search', async () => {
+  it('Should redirect to the search page url with search query param if the user does a search correctly with the search button', async () => {
+    const searchQuery = 'test'
+
+    const navigateMock = vi.fn()
+
     renderWithRouter(<SearchField />)
+
+    useNavigateMock.mockReturnValue(navigateMock)
+
     const user = userEvent.setup()
 
-    const inputSearch = screen.getByRole('search')
+    const inputSearch = screen.getByPlaceholderText('Search')
 
-    await user.click(inputSearch)
+    await user.type(inputSearch, searchQuery)
 
-    const searchIcon = screen.queryByTestId('searchIcon')
-    expect(searchIcon).not.toBeNull()
+    const searchButton = screen.getByRole('button')
+
+    await user.click(searchButton)
+
+    expect(navigateMock).toHaveBeenCalledOnce()
+    expect(navigateMock).toHaveBeenCalledWith(
+      '/search?search_query=' + searchQuery
+    )
+  })
+
+  it('Should redirect to the search page url with search query param if the user does a search correctly with the search button', async () => {
+    const searchQuery = 'test'
+
+    const navigateMock = vi.fn()
+
+    renderWithRouter(<SearchField />)
+
+    useNavigateMock.mockReturnValue(navigateMock)
+
+    const user = userEvent.setup()
+
+    const inputSearch = screen.getByPlaceholderText('Search')
+
+    await user.type(inputSearch, searchQuery)
+
+    await user.keyboard('{enter}')
+
+    expect(navigateMock).toHaveBeenCalledOnce()
+    expect(navigateMock).toHaveBeenCalledWith(
+      '/search?search_query=' + searchQuery
+    )
   })
 })
