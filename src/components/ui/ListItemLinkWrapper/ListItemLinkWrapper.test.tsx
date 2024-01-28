0a640b3ee5b9825bd67793e9ab2b94eb37c9f299
JasonCrk <emerzonkolki@gmail.com>
2024-01-28 10:10:13 -0500

    test(ListItemLinkWrapper.test.tsx): add test

--- src/components/ui/ListItemLinkWrapper/ListItemLinkWrapper.test.tsx
+++ src/components/ui/ListItemLinkWrapper/ListItemLinkWrapper.test.tsx
@@ -1,11 +1,48 @@
 import { cleanup, screen } from '@testing-library/react'
-
-import { render } from '@/utils/testing/render'
+import userEvent from '@testing-library/user-event'
 
 import ListItemLinkWrapper from '@/components/ui/ListItemLinkWrapper'
 
+import { render } from '@/utils/testing/render'
+
+import { useNavigate } from 'react-router-dom'
+
 describe('<ListItemLinkWrapper />', () => {
+  vi.mock('react-router-dom', async () => {
+    const actual = await vi.importActual('react-router-dom')
+    return {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ...(actual as any),
+      useNavigate: vi.fn()
+    }
+  })
+
+  const useNavigateMock = vi.mocked(useNavigate)
+
   afterEach(cleanup)
+
+  it('Should redirect ', async () => {
+    const href = '/test'
+
+    const navigateMock = vi.fn()
+
+    useNavigateMock.mockReturnValue(navigateMock)
+
+    render(
+      <ListItemLinkWrapper href={href}>
+        {isActive => <>{isActive}</>}
+      </ListItemLinkWrapper>
+    )
+
+    const user = userEvent.setup()
+
+    const listItemLink = screen.getByRole('link')
+
+    await user.click(listItemLink)
+
+    expect(navigateMock).toHaveBeenCalledOnce()
+    expect(navigateMock).toHaveBeenCalledWith(href)
+  })
 
   it('Should the href prop equal to href attribute of link', () => {
     const href = '/'
