9f44f0a9fcfdcf53c06c1534492b60e00bfd9683
JasonCrk <emerzonkolki@gmail.com>
2024-01-29 11:33:02 -0500

    test(NavbarAsideIconLink.test.tsx): add new test

--- src/components/ui/NavbarAsideIconLink/NavbarAsideIconLink.test.tsx
+++ src/components/ui/NavbarAsideIconLink/NavbarAsideIconLink.test.tsx
@@ -1,15 +1,29 @@
 import { cleanup, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-import NavbarAsideIconLink from '.'
+import NavbarAsideIconLink from '@/components/ui/NavbarAsideIconLink'
 
 import { render } from '@/utils/testing/render'
 
 import Icon from '@mui/icons-material/Abc'
 
+import { useNavigate } from 'react-router-dom'
+
 const ACTIVE_ICON_ID = 'activeIcon'
 const NO_ACTIVE_ICON_ID = 'noActiveIcon'
 
 describe('<NavbarAsideIconLink />', () => {
+  vi.mock('react-router-dom', async () => {
+    const actual = await vi.importActual('react-router-dom')
+    return {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ...(actual as any),
+      useNavigate: vi.fn()
+    }
+  })
+
+  const useNavigateMock = vi.mocked(useNavigate)
+
   afterEach(cleanup)
 
   it('Should render the component', () => {
@@ -26,7 +40,7 @@ describe('<NavbarAsideIconLink />', () => {
     expect(iconLink).toBeInTheDocument()
   })
 
-  it('Should contain the title prop', () => {
+  it('Should contain the title prop like text content', () => {
     const title = 'test'
 
     render(
@@ -38,8 +52,8 @@ describe('<NavbarAsideIconLink />', () => {
       />
     )
 
-    const elementWithTitleContent = screen.queryByText(title)
-    expect(elementWithTitleContent).not.toBeNull()
+    const titleContent = screen.queryByText(title)
+    expect(titleContent).toBeInTheDocument()
   })
 
   it('Should contain a link', () => {
@@ -56,7 +70,7 @@ describe('<NavbarAsideIconLink />', () => {
     expect(link).toBeInTheDocument()
   })
 
-  it('Should the link have a href attribute equal to the href prop', () => {
+  it('Should the link has a href attribute equal to the href prop', () => {
     const href = '/test'
 
     render(
@@ -68,7 +82,7 @@ describe('<NavbarAsideIconLink />', () => {
       />
     )
 
-    const link = screen.getByRole('link')
+    const link = screen.queryByRole('link')
     expect(link).toHaveAttribute('href', href)
   })
 
@@ -90,7 +104,37 @@ describe('<NavbarAsideIconLink />', () => {
     )
 
     const activeIcon = screen.queryByTestId(ACTIVE_ICON_ID)
-    expect(activeIcon).not.toBeNull()
+    expect(activeIcon).toBeInTheDocument()
+  })
+
+  it('Should navigate to the path indicated by the href prop if the user clicks on it', async () => {
+    const pathname = '/test'
+
+    const navigateMock = vi.fn()
+    useNavigateMock.mockReturnValue(navigateMock)
+
+    render(
+      <NavbarAsideIconLink
+        activeIcon={<Icon data-testid={ACTIVE_ICON_ID} />}
+        noActiveIcon={<Icon data-testid={NO_ACTIVE_ICON_ID} />}
+        title='test'
+        href={pathname}
+      />,
+      {
+        routerOptions: {
+          initialEntries: ['/']
+        }
+      }
+    )
+
+    const user = userEvent.setup()
+
+    const link = screen.getByRole('link')
+
+    await user.click(link)
+
+    expect(navigateMock).toHaveBeenCalledOnce()
+    expect(navigateMock).toHaveBeenCalledWith(pathname)
   })
 
   it("Shouldn't show the active icon when the pathname is not equal to the href prop", () => {
@@ -128,7 +172,7 @@ describe('<NavbarAsideIconLink />', () => {
     )
 
     const noActiveIcon = screen.queryByTestId(NO_ACTIVE_ICON_ID)
-    expect(noActiveIcon).not.toBeNull()
+    expect(noActiveIcon).toBeInTheDocument()
   })
 
   it("Shouldn't show the no active icon when then pathname is equal to the href prop", () => {
