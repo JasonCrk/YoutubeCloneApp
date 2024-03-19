9c0b385dfd9361b9602fd0609d27363cdf8ef953
JasonCrk <emerzonkolki@gmail.com>
2024-03-19 16:16:10 -0500

    refactor(ThumbnailImage/index.tsx): add maxWidth prop

--- src/components/ui/ThumbnailImage/index.tsx
+++ src/components/ui/ThumbnailImage/index.tsx
@@ -4,17 +4,19 @@ interface Props {
   thumbnailUrl: string
   alt: string
   width?: string
+  maxWidth?: string
 }
 
-const ThumbnailImage: FC<Props> = ({ alt, thumbnailUrl, width }) => {
+const ThumbnailImage: FC<Props> = ({ alt, thumbnailUrl, width, maxWidth }) => {
   return (
     <img
       src={thumbnailUrl}
       alt={alt}
       role='img'
       style={{
-        maxWidth: width ?? '500px',
+        maxWidth: maxWidth ?? '500px',
         width: width ?? '100%',
+        height: '100%',
         borderRadius: '15px',
         objectFit: 'cover',
         aspectRatio: 16 / 9
