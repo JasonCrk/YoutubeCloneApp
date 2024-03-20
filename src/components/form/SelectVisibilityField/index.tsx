26ab00d58a41fce1094a78cd2cc7482e692fa001
JasonCrk <emerzonkolki@gmail.com>
2024-03-19 22:46:19 -0500

    feat(SelectVisibilityField/index.tsx): extend props
    Changes:
    - Props extends SelectProps (MaterialUI)
    - `register` props is optional
    - add `disableLabel` prop

--- src/components/form/SelectVisibilityField/index.tsx
+++ src/components/form/SelectVisibilityField/index.tsx
@@ -6,19 +6,26 @@ import { Visibility } from '@/models/types'
 
 import MenuItem from '@/components/ui/MenuItem'
 
-import { FormControl, FormHelperText, InputLabel, Select } from '@mui/material'
+import {
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  Select,
+  type SelectProps
+} from '@mui/material'
 
 import PublicIcon from '@mui/icons-material/Public'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import LinkOutlinedIcon from '@mui/icons-material/LinkOutlined'
 
-interface Props {
+interface Props extends SelectProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  register: UseFormRegister<any>
+  register?: UseFormRegister<any>
   name: string
   defaultValue?: Visibility
   error?: boolean
   errorMessage?: string
+  disableLabel?: boolean
 }
 
 const SelectVisibilityField: FC<Props> = ({
@@ -26,19 +33,25 @@ const SelectVisibilityField: FC<Props> = ({
   name,
   error,
   errorMessage,
-  defaultValue
+  defaultValue,
+  disableLabel,
+  ...selectProps
 }) => {
   return (
     <FormControl variant='standard' error={error}>
-      <InputLabel id='privacy-label' error={error}>
-        Privacy
-      </InputLabel>
+      {!disableLabel && (
+        <InputLabel id='privacy-label' error={error}>
+          Privacy
+        </InputLabel>
+      )}
 
       <Select
         labelId='privacy-label'
-        {...register(name)}
+        name={name}
         error={error}
         defaultValue={defaultValue}
+        {...selectProps}
+        {...(register && register(name))}
       >
         <MenuItem value={Visibility.PUBLIC} startIcon={<PublicIcon />}>
           Public
