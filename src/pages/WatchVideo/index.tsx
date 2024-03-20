6b0dc1ed97a6e415ace3410522a0fc931aca49b5
JasonCrk <emerzonkolki@gmail.com>
2024-03-20 11:58:27 -0500

    feat(pages/WatchVideo/index.tsx): update container and responsive page

--- src/pages/WatchVideo/index.tsx
+++ src/pages/WatchVideo/index.tsx
@@ -31,21 +31,24 @@ const WatchVideoPage: FC = () => {
       <Container
         maxWidth='xl'
         disableGutters={isTheaterViewMode}
-        component='div'
+        component='main'
         sx={{
           mt: '56px',
           display: 'grid',
-          gridTemplateColumns: { xs: '1fr', md: '1fr minmax(30%, 400px)' },
-          color: 'white'
+          gridTemplateColumns: { xs: '1fr', md: '1fr minmax(28.5%, 400px)' },
+          color: 'white',
+          '&.MuiContainer-maxWidthXl': {
+            paddingX: { xs: '24px', md: '40px' }
+          }
         }}
       >
         <Box
-          gridColumn={isTheaterViewMode ? '1 / 3' : undefined}
-          height={isTheaterViewMode ? '78vh' : '100%'}
           width='100%'
+          gridColumn={isTheaterViewMode ? 1 / 3 : undefined}
+          height={isTheaterViewMode ? '78vh' : '100%'}
           bgcolor={isTheaterViewMode ? 'black' : 'transparent'}
           pt={isTheaterViewMode ? 0 : 2.5}
-          pr={isTheaterViewMode ? 0 : 2.5}
+          pr={isTheaterViewMode ? 0 : { xs: 0, md: 2.5 }}
         >
           <VideoPlayer
             videoUrl={video.videoUrl}
@@ -65,7 +68,7 @@ const WatchVideoPage: FC = () => {
         <Box
           sx={{ gridColumnStart: 1, gridRowStart: 2 }}
           pl={isTheaterViewMode ? 2.5 : 0}
-          pr={2.5}
+          pr={{ xs: 0, md: 2.5 }}
           pt={1}
           mb={2}
         >
